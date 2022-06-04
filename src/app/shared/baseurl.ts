b17c2b732455311cbdf2cd74b7f2b4f165a8a6e7
johnhtml <jogerenag@unal.edu.co>
2022-06-04 00:24:56 -0500

    change in baseUrl

--- src/app/shared/baseurl.ts
+++ src/app/shared/baseurl.ts
@@ -1,1 +1,2 @@
-export const baseURL: string = 'http://localhost:8080/';
+//export const baseURL: string = 'http://localhost:8080/';
+export const baseURL: string = 'https://orquidea-accesorios.herokuapp.com/';
